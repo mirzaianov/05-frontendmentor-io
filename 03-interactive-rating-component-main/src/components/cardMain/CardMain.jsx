fe6255236f31595333e82e035854b7fefa102905
msejak <7o.mseJak@gmail.com>
2023-08-31 15:44:52 +0300

    completed mobile main button work

--- 03-interactive-rating-component-main/src/components/cardMain/CardMain.jsx
+++ 03-interactive-rating-component-main/src/components/cardMain/CardMain.jsx
@@ -6,19 +6,20 @@ import RadioButton from '../radioButton/RadioButton';
 import './CardMain.scss';
 
 function CardMain() {
-  const [checked, setChecked] = useState(true);
+  const [radio, setRadio] = useState();
 
-  const onChangeRadio = e => {
-    const { checked } = e.target;
-    console.log('checked', checked);
+  const handleChangeRadio = e => {
+    const { value } = e.target;
 
-    if (checked === true) {
-      setChecked({ checked: false });
-    }
+    setRadio(() => value);
 
-    if (checked === false) {
-      setChecked({ checked: true });
-    }
+    console.log('value ==>', e, value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    console.log('Submitted radio:', radio);
   };
 
   return (
@@ -41,48 +42,40 @@ function CardMain() {
 
       <form
         className='card-main__rating'
-        // onSubmit={handleSubmit}
-      >
-        <div className='card-main__values'>
+        onSubmit={handleSubmit}>
+        <div
+          className='card-main__values'
+          value={radio}
+          onChange={e => handleChangeRadio(e)}>
           <RadioButton
             name='choice'
             id='choice1'
             value='Choice1'
             text='1'
-            onChange={onChangeRadio}
-            checked={checked}
           />
           <RadioButton
             name='choice'
             id='choice2'
             value='Choice2'
             text='2'
-            onChange={onChangeRadio}
-            checked={false}
           />
           <RadioButton
             name='choice'
             id='choice3'
             value='Choice3'
             text='3'
-            onChange={onChangeRadio}
-            checked={false}
           />
           <RadioButton
             name='choice'
             id='choice4'
             value='Choice4'
             text='4'
-            onChange={onChangeRadio}
-            checked={false}
           />
           <RadioButton
             name='choice'
             id='choice5'
             value='Choice5'
             text='5'
-            onChange={onChangeRadio}
-            checked={false}
           />
         </div>
 
